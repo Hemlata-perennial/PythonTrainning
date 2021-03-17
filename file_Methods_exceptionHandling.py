f383282af1249caa7518ad313bc236f2f1a5f8b6
jasskiran <kaurjaskaran12@gmail.com>
2021-03-17 21:10:38 +0530

    exception handling added to file operations

--- file_Methods_exceptionHandling.py
+++ file_Methods_exceptionHandling.py
@@ -1,20 +1,24 @@
 try:
-    f = open('home/sample.txt', encoding = 'utf-8')
-   # do file operations.
+    f = open('sample1.txt',mode='w+', encoding = 'utf-8')
+except:
+  print("An exception occurred while opening file")
+
+else:
+    # do file operations.
     f.write('It is my first file\n')
-   #second line
+    #second line
     f.write('This file\n')
-   #third line
+    #third line
     f.write('contains three lines\n')
 
     data = f.read(19);
     print('Read String is : ', data)
 
-    #Check current position
+        #Check current position
     position = f.tell();
     print('Current file position : ', position)
 
-    #Reposition pointer at the beginning once again
+        #Reposition pointer at the beginning once again
     position = f.seek(0, 0);
     data = f.read(19);
     print('Again read String is : ', data)
