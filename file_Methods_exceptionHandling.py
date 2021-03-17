e623d8bfdf0cf3093095267de66925a1a1e9573d
Hemlata-perennial <79569441+Hemlata-perennial@users.noreply.github.com>
2021-03-17 21:24:53 +0530

    Update file_Methods_exceptionHandling.py

--- file_Methods_exceptionHandling.py
+++ file_Methods_exceptionHandling.py
@@ -1,23 +1,29 @@
 try:
-    f = open('home/sample.txt', encoding = 'utf-8')
-   # do file operations.
+    f = open('sample1.txt',mode='r', encoding = 'utf-8')
+except:
+  print("An exception occurred while opening file")
+
+else:
+    # do file operations.
     f.write('It is my first file\n')
-   #second line
+    #second line
     f.write('This file\n')
-   #third line
+    #third line
     f.write('contains three lines\n')
 
-    data = f.read(19);
+    data = f.read(19)
     print('Read String is : ', data)
 
-    #Check current position
-    position = f.tell();
+        #Check current position
+    position = f.tell()
     print('Current file position : ', position)
 
-    #Reposition pointer at the beginning once again
-    position = f.seek(0, 0);
+        #Reposition pointer at the beginning once again
+    position = f.seek(0, 0)
     data = f.read(19);
     print('Again read String is : ', data)
 finally:
    f.close()
+
+
 
